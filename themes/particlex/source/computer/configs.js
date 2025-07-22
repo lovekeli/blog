dca90d809fe2f425fe040d71978245fdbec19b29
lichuang <debug_log@163.com>
2025-07-22 13:45:58 +0800

    lc
    新增ssh

--- themes/particlex/source/computer/configs.js
+++ themes/particlex/source/computer/configs.js
@@ -14,7 +14,7 @@ YL.static = {
   /** YLUI基础设置 */
   lang: 'zh-cn', //语言
   localStorageName: "system", //ls存储名
-  lockedApps: ['yl-system', 'yl-color-picker', 'yl-browser','lctab',"app-music","app-games","app-wallpaperstore","app-note","app-pdf","app-excel","app-pets","yl-color","githubproxy","reader","medium"], // 锁定的应用（不允许被脚本修改）
+  lockedApps: ['yl-system', 'yl-color-picker', 'yl-browser','lctab',"app-music","app-games","app-wallpaperstore","app-note","app-pdf","app-excel","app-pets","yl-color","githubproxy","reader","medium","webssh"], // 锁定的应用（不允许被脚本修改）
   trustedApps: [], // 受信任的应用（可以使用敏感API）
   debug: false,//启用更多调试信息
   beforeOnloadEnable: false,//启用关闭前询问（打包app时请关闭防止出错）
